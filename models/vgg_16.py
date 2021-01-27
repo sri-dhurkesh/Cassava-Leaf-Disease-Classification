910fd1a60e5a89957509a114611e62b17afa6501
sri-dhurkesh <svdhurkesh68@gmail.com>
2021-01-27 12:02:36 +0530

    vgg-16 model

---
+++ models/vgg_16.py
@@ -0,0 +1,47 @@
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Input
+from tensorflow.keras import Model
+from tensorflow.keras.utils import get_file
+
+
+def vgg16(img_input=(224, 224, 3), path_url=None):
+    # Block 1
+    input = Input(shape=img_input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(input)
+    x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
+    x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
+    x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
+    x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    model = Model(input, x, name='vgg16')
+    path = get_file('/media/sri/shared/SEM-8/cassava leaf disease classification/vgg16_without_top.h5', path_url,
+                    cache_dir='/media/sri/shared/SEM-8/cassava leaf disease classification/cache')
+
+    model.load_weights(path)
+    model.summary()
+
+    return model
+
+
+m = vgg16(img_input=(228, 228, 3),
+          path_url='https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5')
